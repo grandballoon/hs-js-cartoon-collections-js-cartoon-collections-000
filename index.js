8d7bed6347901dd92edc39d03552b36512739067
Luke Conley <luke.sovern.conley@gmail.com>
2018-05-09 22:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+var dwarves = ['Dopey','Happy','Grumpy','Doc']
 function dwarfRollCall(dwarves) {
+  var i;
+  for (i = 0, i < dwarves.length, i++)
+    {
+  var number = dwarves.indexOf(i) + 1
+  return number 
+    }
 }
 
 function summonCaptainPlanet(planeteerCalls){
